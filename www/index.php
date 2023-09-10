66cce4a436970b94c0fe7fac2d36fe8731754e19
LuigiZanon <lugazabugre@outlook.com>
2023-09-10 17:31:15 -0300

    added css into the project

--- www/index.php
+++ www/index.php
@@ -5,24 +5,39 @@ session_start();
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="formulario.css" media="screen"></link>
         <title>CRUD - Cadastrar</title>
     </head>
-    <body>
-        <h1>Cadastrar Usuários</h1>
+    <body> 
+        <div>
+            <h1 id="titulo">Cadastrar Usuário</h1>
+            <p id="subtitulo"> Insira suas informações</p>
+        </div>
+
         <?php
-        if(isset($_SESSION['msg'])){
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-        }
+            if(isset($_SESSION['msg'])){
+                echo $_SESSION['msg'];
+                unset($_SESSION['msg']);
+            }
         ?>
+
         <form method="POST" action="processa.php">
-            <label>Nome: <label>
-            <input type="text" name="nome" placeholder="Digite o nome completo"><br><br>
 
-            <label>Email: <label>
-            <input type="email" name="email" placeholder="Digite o seu email"><br><br>
+            <fieldset class="nome">
+                <div>
+                    <label for="nome"><strong>Nome:</strong><label>
+                    <input type="text" name="nome" placeholder="Digite o nome completo" required><br><br>
+                </div>
+            </fieldset>
 
-            <input type="submit" value="Cadastrar">
+            <fieldset class="email">
+                <div>
+                    <label for="email"><strong>Email:<strong><label>
+                    <input type="email" name="email" placeholder="Digite o seu email" required><br><br>
+                </div>
+            </fieldset>
+
+            <button class="botao" type="submit">Concluído</button>
         </form>
     </body>
 </html>
